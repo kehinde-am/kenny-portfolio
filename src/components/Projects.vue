1d1917ac4213bc55733683f1a125d844d8c2ae1a
Kehindeee <k.h.amusa@wlv.ac.uk>
2024-05-23 21:22:21 +0100

    update: projects details

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,9 +3,9 @@
     <h2 class="text-4xl font-bold text-center text-blue-700">Projects</h2>
     <p class="text-center text-blue-700 mt-4 mb-12">Details about my projects...</p>
     <div class="container mx-auto px-4">
-      <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-8">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <div v-for="project in projects" :key="project.id" class="bg-white p-6 rounded-lg shadow-lg">
-          <h3 class="text-2xl font-bold mb-4 text-blue-700">{{ project.title }}</h3>
+          <h3 class="text-2xl font-bold mb-4 text-black">{{ project.title }}</h3>
           <img :src="project.image" alt="Project image" class="w-full h-48 object-cover mb-4 rounded-lg">
           <div class="flex flex-wrap mb-4">
             <span v-for="tech in project.technologies" :key="tech" class="bg-gray-200 text-blue-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
